4e910a0b8986cc50e5e329963b6dfc1b9ff89e5a
oshou <adf1985adf@gmail.com>
2014-11-18 19:48:32 +0900

    all

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DOT_FILES=(.bash_logout .bash_profile .bashrc .cshrc .gitconfig .gitignore .tcshrc .tmux.conf .vimrc)
+
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
