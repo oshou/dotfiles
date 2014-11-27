a7800651316079cb3b0538487447d80409849c6c
oshou <adf1985adf@gmail.com>
2014-11-27 22:57:04 +0900

    update

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ DOT_FILES=(.bash_logout .bash_profile .bashrc .cshrc .gitconfig .gitignore .tcsh
 for file in ${DOT_FILES[@]}
 do
     ln -sf $HOME/dotfiles/$file $HOME/$file
+    echo "make link $HOME/dotfiles/$file -> $HOME/$file"
 done
+echo "setup is done!"
