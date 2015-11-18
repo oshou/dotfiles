db30d81a7603835130e7a893098d9bcda9418d25
oshou <adf1985adf@gmail.com>
2015-11-18 21:22:32 +0900

    modify .bashrc

--- setup.sh
+++ setup.sh
@@ -7,9 +7,12 @@ do
     ln -sf $HOME/dotfiles/$file $HOME/$file
     echo "make link $HOME/dotfiles/$file -> $HOME/$file"
 done
+source .bashrc
 
+# Download Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Download Message Bundles
 echo "Please open Vim && do BundleInstall"
 
 echo "setup is complete!"
