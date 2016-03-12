8312b2ecab15bbb3e4b8c20518ae7e1e30406fdb
oshou <adf1985adf@gmail.com>
2016-03-12 23:00:52 +0900

    modify install.sh

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-backupdir=/tmp/backup
-backupdate=`date +%Y%m%d`
-backupdatedir=${backupdir}/${backupdate}
-mkdir ${backupdir}
-mkdir ${backupdatedir}
-echo "Start backup for ${backupdate}"
-rsync -aur --delete /root/rails_pj/ ${backupdatedir}
-echo "Complete backup for ${backupdate}!!"
