c8c2434460d87cf783faed40300241c8f8eaa3d2
oshou <adf1985adf@gmail.com>
2022-11-26 23:57:22 +0900

    post

--- setup.sh
+++ setup.sh
@@ -16,8 +16,8 @@ mkdir -p ${DOTFILES_DIR}/backup
 for file in ${DOTFILES[@]}
 do
   if [[ -e ~/$file ]];then
-    echo "* backup $HOME/$file -> ${DOTFILES_DIR}/backup/$file.bak_`date +%y%m%d-%H%m%S`"
-    cp ~/$file ${DOTFILES_DIR}/backup/$file.bak_`date +%y%m%d-%H%m%S`
+    echo "* backup $HOME/$file -> ${DOTFILES_DIR}/backup/$file.bak_`date +%Y%m%d-%H%m%S`"
+    cp ~/$file ${DOTFILES_DIR}/backup/$file.bak_`date +%Y%m%d-%H%m%S`
   fi
   echo "* create Link ${DOTFILES_DIR}/$file -> ~/$file"
   ln -sf ${DOTFILES_DIR}/$file ~/$file
